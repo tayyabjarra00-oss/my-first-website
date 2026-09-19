6c58492d6caa8a0d30a45c606e84a1c0949a864f
Tayyab Jarra <YOUR-GITHUB-EMAIL>
2026-09-19 11:21:57 -0700

    Initial NOVA website

---
+++ script.js
@@ -0,0 +1,8 @@
+const startButton = document.getElementById("startButton");
+const message = document.getElementById("message");
+
+startButton.addEventListener("click", function () {
+
+    message.textContent = "Welcome to NOVA! Your journey has started 🚀";
+
+});
